1df1e0b4451fe09849e9eeb234a31a1c3b8470b4
agajankush <agajankush.github@gmail.com>
2025-07-22 23:27:02 -0400

    MongoDB atlas connection

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require("dotenv").config({ path: "./.env" });
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./.env" });
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
+// This is an iffy logic just another way of writing an async function
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (err) => {
+            console.error("Server error:", err);
+            throw err;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        throw err;
+    }
+})();
+*/
