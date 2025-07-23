92af06cd7a24da20f81bb0ca9aa11cac015b8581
agajankush <agajankush.github@gmail.com>
2025-07-23 00:40:48 -0400

    utils code for basic handling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 // require("dotenv").config({ path: "./.env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" }); // Load environment variables from .env file in whole project
 
-connectDB(); // connect to MongoDB
+// connect to MongoDB
+// Since we used aysnc function to connect to MongoDB, we can use .then() and .catch() to handle the promise
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1); // Exit the process with failure
+    });
 
 /*
 import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) =>
+            next(err)
+        );
+    };
+};
+
+export { asyncHandler };
+
+// Higher order function used to handle async errors in Express middleware
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error",
+//         });
+//     }
+// };
